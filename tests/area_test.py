590606d8757c537413f68eb7d8720319176e7eaa
Kadiri Ehijie <Prosper.ehijie@ab-inbev.com>
2025-07-03 15:11:43 +0100

    added test and updated workflow

--- tests/area_test.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from codes.calculator import calculate_area_of_circle, calculate_area_of_rectangle, calculate_area_of_square
-
-def test_calculate_area_circle():
-    assert calculate_area_of_circle(3.142, 12) == (3.142 * (12 ** 2))
